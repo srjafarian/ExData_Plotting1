498454942d9240c003e06f2275d620d65a56e16d
Scott <51201969+srjafarian@users.noreply.github.com>
2019-06-19 13:43:13 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,7 @@
+png(filename = "polt1.png", width = 480, height = 480)
+power1 <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power1) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subs <- subset(power1,power1$Date=="1/2/2007" | power1$Date =="2/2/2007")
+hist(as.numeric(as.character(subs$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power")
+dev.off()
